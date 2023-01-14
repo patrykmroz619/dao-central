58c1730a27512d9c46c68524ea9e8028a68a6a25
Patryk Mróz <patrykmroz619@wp.pl>
2023-01-14 16:00:05 +0100

    chore: deploy nft voting factory contract on the mumbai testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-abi-exporter";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const fallbackKey =
+  "0000000000000000000000000000000000000000000000000000000000000000";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: `https://rpc.ankr.com/polygon_mumbai`,
+      accounts: [process.env.PRIVATE_KEY || fallbackKey],
+    },
+  },
   abiExporter: {
     runOnCompile: true,
   },
