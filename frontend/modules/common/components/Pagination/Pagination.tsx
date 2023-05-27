7c59120a0acd139b132ad72e59c943bbf2ee150f
Patryk Mróz <patrykmroz619@wp.pl>
2023-05-27 18:26:40 +0200

    feat(frontend-component): create pagination component

---
+++ frontend/modules/common/components/Pagination/Pagination.tsx
@@ -0,0 +1,36 @@
+import { ComponentPropsWithoutRef } from "react";
+import ReactPaginate from "react-paginate";
+
+import styles from "./Pagination.module.scss";
+
+type PaginationProps = {
+  currentPage: number;
+  pageCount: number;
+  onPageChange: (page: number) => void;
+} & ComponentPropsWithoutRef<"div">;
+
+export const Pagination = (props: PaginationProps) => {
+  const { currentPage, pageCount, onPageChange, className, ...restProps } =
+    props;
+
+  return (
+    <div className={className} {...restProps}>
+      <ReactPaginate
+        containerClassName={styles.container}
+        pageClassName={styles.page}
+        activeClassName={styles.activePage}
+        breakClassName={styles.break}
+        previousClassName={styles.previous}
+        nextClassName={styles.next}
+        pageRangeDisplayed={3}
+        previousLabel="<"
+        nextLabel=">"
+        pageCount={pageCount}
+        forcePage={currentPage - 1}
+        onPageChange={({ selected }) => {
+          onPageChange(selected + 1);
+        }}
+      />
+    </div>
+  );
+};
